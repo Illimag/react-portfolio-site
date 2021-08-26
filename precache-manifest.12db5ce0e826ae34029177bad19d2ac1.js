12a1259172aec8e37de4a040fb884a8ff1b81050
uiuxdeveloper-io <uiuxdeveloper@comcast.net>
2021-08-25 19:03:23 -0700

    Updates

---
+++ precache-manifest.12db5ce0e826ae34029177bad19d2ac1.js
@@ -0,0 +1,286 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "765dc99e28c663d5fb863011cc6fec54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94c1148edcc3a8ff4a8e",
+    "url": "/static/css/2.346de7d8.chunk.css"
+  },
+  {
+    "revision": "7352899e83bafc886c0f",
+    "url": "/static/css/main.afb37f54.chunk.css"
+  },
+  {
+    "revision": "94c1148edcc3a8ff4a8e",
+    "url": "/static/js/2.a2bb0189.chunk.js"
+  },
+  {
+    "revision": "a928f9ede91887d6435d72af55ca8770",
+    "url": "/static/js/2.a2bb0189.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7352899e83bafc886c0f",
+    "url": "/static/js/main.2f3af4f6.chunk.js"
+  },
+  {
+    "revision": "ac85d4e4ba0d56af43d5",
+    "url": "/static/js/runtime-main.82579a8e.js"
+  },
+  {
+    "revision": "09f2faffaab3b971821942794d628bac",
+    "url": "/static/media/1-01.09f2faff.jpg"
+  },
+  {
+    "revision": "6a4736779c220aa63fb885714a92cc9e",
+    "url": "/static/media/1-01.6a473677.jpg"
+  },
+  {
+    "revision": "8afa113084a114c1f3694d37f18192e5",
+    "url": "/static/media/1-01.8afa1130.jpg"
+  },
+  {
+    "revision": "d4d0b979dd0059d9620b97284375cc6f",
+    "url": "/static/media/1-01.d4d0b979.jpg"
+  },
+  {
+    "revision": "1170e406c4dc9412d69017437a46559b",
+    "url": "/static/media/1-02.1170e406.jpg"
+  },
+  {
+    "revision": "1dda5d802741a855c28caaca3e920798",
+    "url": "/static/media/1-02.1dda5d80.jpg"
+  },
+  {
+    "revision": "5f6b7d81b6888d9c29800bf9b9533e13",
+    "url": "/static/media/1-02.5f6b7d81.jpg"
+  },
+  {
+    "revision": "6c1999d620628fe67d57d6716d9a308a",
+    "url": "/static/media/1-02.6c1999d6.jpg"
+  },
+  {
+    "revision": "abb2c248cd05df38906c9a080745389d",
+    "url": "/static/media/1-02.abb2c248.jpg"
+  },
+  {
+    "revision": "1d0be93f03849c0c8a88d9a385cf0e5d",
+    "url": "/static/media/1-03.1d0be93f.jpg"
+  },
+  {
+    "revision": "454b4828718db94fe107c4a41b7619a4",
+    "url": "/static/media/1-03.454b4828.jpg"
+  },
+  {
+    "revision": "5e7157ea16682dd3ef7c15e339b6bcc2",
+    "url": "/static/media/1-03.5e7157ea.jpg"
+  },
+  {
+    "revision": "a663aa56d5621c6a97077e40d5a6b78b",
+    "url": "/static/media/1-03.a663aa56.jpg"
+  },
+  {
+    "revision": "d4ee82902545273c8f95fc904691b4f2",
+    "url": "/static/media/1-03.d4ee8290.jpg"
+  },
+  {
+    "revision": "68b140ede6504efb9f324bed471aca7d",
+    "url": "/static/media/1-04.68b140ed.jpg"
+  },
+  {
+    "revision": "6aadd9b9c646c66a265903ff9ce8f8d0",
+    "url": "/static/media/1-04.6aadd9b9.jpg"
+  },
+  {
+    "revision": "99fbcb7f1f5ec5548e2cf76a80f36e4f",
+    "url": "/static/media/1-04.99fbcb7f.jpg"
+  },
+  {
+    "revision": "c28e62b5943610aaa73518ad14f0217e",
+    "url": "/static/media/1-04.c28e62b5.jpg"
+  },
+  {
+    "revision": "c33cbd3860874168bbd485d2b371433b",
+    "url": "/static/media/1-04.c33cbd38.jpg"
+  },
+  {
+    "revision": "0a46f9b8934eade26d09f463bbde18b0",
+    "url": "/static/media/1-05.0a46f9b8.jpg"
+  },
+  {
+    "revision": "a465fc07449c012398e592da4463fe15",
+    "url": "/static/media/1-05.a465fc07.jpg"
+  },
+  {
+    "revision": "b5ddcc2176a2fb06e94276e8f2c22d3f",
+    "url": "/static/media/1-05.b5ddcc21.jpg"
+  },
+  {
+    "revision": "d45f8ca40ed1435ba0204a9431acd7a1",
+    "url": "/static/media/1-05.d45f8ca4.jpg"
+  },
+  {
+    "revision": "f4eff766595bc28d6bdf2d440b328ef1",
+    "url": "/static/media/1-05.f4eff766.jpg"
+  },
+  {
+    "revision": "1ccbb6bc41d0e7544752efa55de9f22e",
+    "url": "/static/media/1-06.1ccbb6bc.jpg"
+  },
+  {
+    "revision": "510c2669421bcf26a8ae27d57413f544",
+    "url": "/static/media/1-06.510c2669.jpg"
+  },
+  {
+    "revision": "69ebe97b4cb83c0fd52d7258b96cde4f",
+    "url": "/static/media/1-06.69ebe97b.jpg"
+  },
+  {
+    "revision": "a74a6eb573c27628973e8a594fc1eb05",
+    "url": "/static/media/1-06.a74a6eb5.jpg"
+  },
+  {
+    "revision": "b326dbff02cf9587d9b79681ed8bdbda",
+    "url": "/static/media/1-06.b326dbff.jpg"
+  },
+  {
+    "revision": "2f4859ba5de5fc3dbfb388835907bc29",
+    "url": "/static/media/1-07.2f4859ba.jpg"
+  },
+  {
+    "revision": "6ecb138b8b67f03127aed16887e033fa",
+    "url": "/static/media/1-07.6ecb138b.jpg"
+  },
+  {
+    "revision": "a68abe354b92138c47d61b2e855e94c4",
+    "url": "/static/media/1-07.a68abe35.jpg"
+  },
+  {
+    "revision": "c8b138696417f5ec29ed4fd3a0170e41",
+    "url": "/static/media/1-07.c8b13869.jpg"
+  },
+  {
+    "revision": "f55bf7179a5bacbc4fdfa6769e2e51d9",
+    "url": "/static/media/1-07.f55bf717.jpg"
+  },
+  {
+    "revision": "04c242c0d0f579ec077560baa25e446c",
+    "url": "/static/media/1-08.04c242c0.png"
+  },
+  {
+    "revision": "4da2667657e1378cfb38d270b59352ee",
+    "url": "/static/media/1-08.4da26676.jpg"
+  },
+  {
+    "revision": "b6dd79da7eb202834942961416563f6b",
+    "url": "/static/media/1-08.b6dd79da.jpg"
+  },
+  {
+    "revision": "bbfed51b94bd024e543cb4562cfaee97",
+    "url": "/static/media/1-08.bbfed51b.jpg"
+  },
+  {
+    "revision": "da315934cf80dae4518d576a53b720b9",
+    "url": "/static/media/1-09.da315934.jpg"
+  },
+  {
+    "revision": "fd412ad2aaaa940c587d96b855b5a12d",
+    "url": "/static/media/1-09.fd412ad2.png"
+  },
+  {
+    "revision": "4072dad5b4a3601aa9fa70e8f1eede94",
+    "url": "/static/media/aerial-view-of-san-francisco-at-night-PDXCT9H.4072dad5.jpg"
+  },
+  {
+    "revision": "e2853d31c59b9797fa6da222c34244e8",
+    "url": "/static/media/banner-02.e2853d31.jpg"
+  },
+  {
+    "revision": "17895d0869575982a50df6e2a59c6424",
+    "url": "/static/media/banner-03.17895d08.jpg"
+  },
+  {
+    "revision": "9084536459e20efc03de76429ec64c09",
+    "url": "/static/media/banner-04.90845364.jpg"
+  },
+  {
+    "revision": "928c6ff2a152a4830c1a6315bafb594a",
+    "url": "/static/media/banner-06.928c6ff2.jpg"
+  },
+  {
+    "revision": "76ec8c31648dfed4fd003c4bd7f6299a",
+    "url": "/static/media/compress_sf.76ec8c31.mp4"
+  },
+  {
+    "revision": "3c375be06a67bece900c5c2c322a67cc",
+    "url": "/static/media/compress_vid0.3c375be0.mp4"
+  },
+  {
+    "revision": "74df8d38e158a0a0f3f94abee83cc347",
+    "url": "/static/media/compress_vid1.74df8d38.mp4"
+  },
+  {
+    "revision": "44731a68078d9816488d381d624cb26b",
+    "url": "/static/media/compress_vid2.44731a68.mp4"
+  },
+  {
+    "revision": "50f3850cb59e2e6035cba6e136f085ca",
+    "url": "/static/media/compress_vid4.50f3850c.mp4"
+  },
+  {
+    "revision": "40914bb9734deb857066be6035da0b58",
+    "url": "/static/media/compress_vid5.40914bb9.mp4"
+  },
+  {
+    "revision": "65b952ab81a97afe64c9a903c9925168",
+    "url": "/static/media/compress_vid6.65b952ab.mp4"
+  },
+  {
+    "revision": "68ba829d7cf30a53f231c93e1922ecaa",
+    "url": "/static/media/customer_location.68ba829d.PNG"
+  },
+  {
+    "revision": "fed78b53edd9c8adc2fb8388c89ddc8c",
+    "url": "/static/media/ordersize.fed78b53.PNG"
+  },
+  {
+    "revision": "d564674221f62d556c35a3cfb5001b80",
+    "url": "/static/media/topcustromerbysalers.d5646742.PNG"
+  },
+  {
+    "revision": "35deab609c4e1281a6faf740736b616f",
+    "url": "/static/media/transaction.35deab60.PNG"
+  },
+  {
+    "revision": "41db86d5f5125ff2bab8740bff8085a8",
+    "url": "/static/media/vid0.41db86d5.mp4"
+  },
+  {
+    "revision": "80db2340da4308069abda394674835e5",
+    "url": "/static/media/vid2.80db2340.m4v"
+  },
+  {
+    "revision": "a46fa44d953538a64d7f1fcd82aef2f2",
+    "url": "/static/media/vid2.a46fa44d.mp4"
+  },
+  {
+    "revision": "47b99e12ad142864269b9f95430f0f21",
+    "url": "/static/media/vid3.47b99e12.mp4"
+  },
+  {
+    "revision": "8b6e96266b55b8a53ed8ab7a4395a603",
+    "url": "/static/media/vid5.8b6e9626.mp4"
+  },
+  {
+    "revision": "8b2763f78786d7458b29623e8179a10b",
+    "url": "/static/media/videotest.8b2763f7.mp4"
+  },
+  {
+    "revision": "4723d94b110290185288387040d5d9b2",
+    "url": "/static/media/volumne.4723d94b.PNG"
+  },
+  {
+    "revision": "86ad9339f4c440c4fa04c33eeb6b7948",
+    "url": "/static/media/vr_sitting.86ad9339.mp4"
+  }
+]);
