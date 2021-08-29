9ce7eefc58576b2b1fc92762357a89c15f3ee45b
uiuxdeveloper-io <uiuxdeveloper@comcast.net>
2021-08-28 20:21:36 -0700

    compressed home vids

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6cf27443ac25ec03bfea6210a0fe1cf9.js"
+  "/precache-manifest.a8d1e753d40cebe3ebfdc565a24c8e18.js"
 );
 
 self.addEventListener('message', (event) => {
