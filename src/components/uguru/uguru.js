697389ff56ac1ab6b8761eb9bb8199bc2bcf8497
uiuxdeveloper-io <uiuxdeveloper@comcast.net>
2021-10-06 19:05:29 -0700

    changed buttons

--- src/components/uguru/uguru.js
+++ src/components/uguru/uguru.js
@@ -702,14 +702,14 @@ export const Uguru = () => (
 <Card className={styles.cardborder}>
     
 <div className={styles.spaceing}></div>
-<p className={styles.descriptionlargewhitebold}>Sanchez Coffee Co.</p>
+<p className={styles.descriptionlargewhitebold}>Resume</p>
 
-  <p className={styles.descriptionlargewhite}>Mobile App Redesign</p>
+  <p className={styles.descriptionlargewhite}>Are you looking for a resume?</p>
 
 
-  <LinkContainer to="/sanchezcoffeeco">
+  <LinkContainer to="/Contact">
                 <p>
-                  <Button className={styles.mainbutton} variant="primary">View Next</Button>
+                  <Button className={styles.mainbutton} variant="primary">View Resume</Button>
                 </p>
               </LinkContainer>
 
