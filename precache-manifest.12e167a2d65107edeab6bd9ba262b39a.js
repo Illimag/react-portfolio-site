0a687f290686580e1e8b09616483c23777bc8bb1
uiuxdeveloper-io <uiuxdeveloper@comcast.net>
2021-12-13 11:04:03 -0800

    Updates

--- precache-manifest.12e167a2d65107edeab6bd9ba262b39a.js
+++ precache-manifest.12e167a2d65107edeab6bd9ba262b39a.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6b82efb233bb4b098517e0312217a97f",
+    "revision": "1e751f161106ae43868e9f4d853659a8",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.46b578d6.chunk.css"
   },
   {
-    "revision": "fe205400a92c5f18bd7a",
+    "revision": "478385e0c3dc6a321f28",
     "url": "/static/css/main.19033774.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.a4f81aeb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fe205400a92c5f18bd7a",
-    "url": "/static/js/main.e867d830.chunk.js"
+    "revision": "478385e0c3dc6a321f28",
+    "url": "/static/js/main.630c285f.chunk.js"
   },
   {
     "revision": "58c15806c185c823953f",
