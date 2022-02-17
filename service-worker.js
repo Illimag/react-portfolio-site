97eaa60a464dfa5725cb58db2f1113e4ac283933
root <root@DESKTOP-UH5HORK.localdomain>
2022-02-16 22:43:41 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.20854edda91a2285770d1af7b3912955.js"
+  "/precache-manifest.009005c52cda4730473f1f2ee7597ef8.js"
 );
 
 self.addEventListener('message', (event) => {
