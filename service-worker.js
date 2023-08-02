181ffc2c4d1739cd24d95c11fba3d54b3737dc38
hillosoft <hillodesign.com@gmail.com>
2023-08-01 23:18:13 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.701ad187afafa8a6aacb5ef22a307413.js"
+  "/precache-manifest.350f45ed2dfaa9cb23627bd56f48fd79.js"
 );
 
 self.addEventListener('message', (event) => {
